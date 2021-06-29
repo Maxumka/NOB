9bdc7901efda5f7fd227ece53ace9b5f1df12251
Maxumka <maxumka1792@gmail.com>
2021-06-29 19:08:54 +0700

    add example

---
+++ NOB.SampleProject/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,16 @@
+﻿using GeneratedAttribute;
+
+namespace NOB.SampleProject.ViewModels
+{
+    [ViewModel]
+    public partial class MainWindowViewModel
+    {
+        [Property]
+        private string text1;
+
+        [Command]
+        private void ClearText1() => PropertyText1 = "";
+
+        public MainWindowViewModel() { }
+    }
+}
